16f92c0bd33ca265a2ad6d7683697e188bc32d6f
nitesh-dev <niteshdev547@gmail.com>
2023-12-24 10:04:19 +0530

    fixed

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,48 @@ import {
   getTotalFileLength,
 } from "./assetsTextures";
 
+class Sprite extends PIXI.AnimatedSprite {
+  to = -1;
+  isPlay = false;
+  deltaCounter = 0;
+  timeToReach=1
+  constructor(textures: PIXI.Texture[], autoUpdate?: boolean) {
+    super(textures, autoUpdate);
+  }
+
+  playTo(to: number) {
+    this.to = to;
+    this.isPlay = true;
+  }
+  stop(): void {
+    super.stop();
+    this.isPlay = false;
+    this.to = -1;
+  }
+  updateMe(delta: number) {
+    if (this.to < 0 || !this.isPlay || this.currentFrame == this.to) {
+      this.isPlay = false;
+      return;
+    }
+
+    // console.log(
+    //   this.to,
+    //   this.isPlay,
+    //   this.currentFrame,
+    //   this.deltaCounter,
+    //   delta
+    // );
+    if (this.deltaCounter > this.timeToReach) {
+      this.deltaCounter = 0;
+
+      // inc or dec frame
+      const incDec = this.currentFrame < this.to ? 1 : -1;
+      this.currentFrame += incDec;
+    }
+    this.deltaCounter += delta * this.totalFrames;
+  }
+}
+
 // resize the canvas
 let canvasContainer: HTMLDivElement | null = null;
 
@@ -83,7 +125,7 @@ let isTestingMode = true;
 let gameState: "ideal" | "intro" | "race" | "finish-line" | "finish" | "final" =
   "ideal";
 
-const gameAnimSpeed = 0.3;
+const gameAnimSpeed = 0.2;
 
 // ---------------------------------------------- objects ------------------------------------------------------------
 
@@ -92,7 +134,6 @@ let introTextures: any;
 let finishLineTextures: any;
 let raceLoopTextures: any;
 let raceLoopRocketTextures = new Array();
-
 let finishBackTextures: any;
 let finishRocketTextures = new Array();
 
@@ -411,87 +452,17 @@ let playerRanks = Array<PlayerRank>();
 export function setupPlayers(ranks: Array<PlayerRank>) {
   playerRanks = ranks.slice();
 }
-
-let raceLoopRocketsArr = new Array<{
+type RocketInfo = {
   player: PlayerRank;
-  rocket: PIXI.AnimatedSprite;
+  rocket: Sprite;
   stopFrame: number;
   oldDirection: "up" | "down";
-}>();
+  isRev?: boolean;
+};
+let raceLoopRocketsArr = new Array<RocketInfo>();
 let isRocketRankChangeAllowed = false;
 
 // max index 29
-export function updateGamePlayerRanks(ranks: Array<PlayerRank>) {
-  if (!isRocketRankChangeAllowed) {
-    print("Rank change not allowed");
-    return;
-  }
-
-  const frameInv = Math.floor(30 / ranks.length);
-
-  for (const element of raceLoopRocketsArr) {
-    const index = ranks.findIndex((item) => item.name == element.player.name);
-    if (index != -1) {
-      const gotoFrame = (ranks.length - ranks[index].rank) * frameInv;
-
-      let direction: "up" | "down" = "up";
-      if (element.rocket.currentFrame > gotoFrame) direction = "down";
-
-      let textures = Array();
-
-      element.player.rank = ranks[index].rank;
-
-      if (element.player.className == "rocket-1") {
-        textures = raceLoopRocketTextures[0].slice();
-      } else if (element.player.className == "rocket-2") {
-        textures = raceLoopRocketTextures[1].slice();
-      } else if (element.player.className == "rocket-3") {
-        textures = raceLoopRocketTextures[2].slice();
-      } else if (element.player.className == "rocket-4") {
-        textures = raceLoopRocketTextures[3].slice();
-      } else if (element.player.className == "rocket-5") {
-        textures = raceLoopRocketTextures[4].slice();
-      } else if (element.player.className == "rocket-6") {
-        textures = raceLoopRocketTextures[5].slice();
-      } else if (element.player.className == "rocket-7") {
-        textures = raceLoopRocketTextures[6].slice();
-      } else if (element.player.className == "rocket-8") {
-        textures = raceLoopRocketTextures[7].slice();
-      } else if (element.player.className == "rocket-9") {
-        textures = raceLoopRocketTextures[8].slice();
-      } else if (element.player.className == "rocket-10") {
-        textures = raceLoopRocketTextures[9].slice();
-      }
-
-      // console.log(element.player.name, element.stopFrame, gotoFrame);
-
-      console.log(direction);
-      if (direction == "up") {
-        element.rocket.textures = textures;
-        if (element.oldDirection == "down") {
-          element.rocket.gotoAndStop(
-            element.rocket.totalFrames - 1 - element.rocket.currentFrame
-          );
-          element.oldDirection = "up";
-        }
-        element.stopFrame = gotoFrame;
-        element.rocket.play();
-      } else {
-        element.rocket.textures = textures.reverse();
-        if (element.oldDirection == "up") {
-          element.rocket.gotoAndStop(
-            element.rocket.totalFrames - 1 - element.rocket.currentFrame
-          );
-          element.oldDirection = "down";
-        }
-        element.stopFrame = element.rocket.totalFrames - 1 - gotoFrame;
-        element.rocket.play();
-      }
-
-
-    }
-  }
-}
 
 function createDeleteRaceLoopGroup(isDelete: boolean) {
   isRocketRankChangeAllowed = false;
@@ -529,42 +500,37 @@ function createDeleteRaceLoopGroup(isDelete: boolean) {
   const halfH = raceLoopContainer.height / 2;
 
   const rocketY = halfH - 100;
-  const rocket1 = new PIXI.AnimatedSprite(raceLoopRocketTextures[0]);
+  const rocket1 = new Sprite(raceLoopRocketTextures[0]);
   rocket1.x = -halfW + 200;
 
-  const rocket2 = new PIXI.AnimatedSprite(raceLoopRocketTextures[1]);
+  const rocket2 = new Sprite(raceLoopRocketTextures[1]);
   rocket2.x = -500;
 
-  const rocket3 = new PIXI.AnimatedSprite(raceLoopRocketTextures[2]);
+  const rocket3 = new Sprite(raceLoopRocketTextures[2]);
   rocket3.x = -360;
 
-  const rocket4 = new PIXI.AnimatedSprite(raceLoopRocketTextures[3]);
+  const rocket4 = new Sprite(raceLoopRocketTextures[3]);
   rocket4.x = -220;
 
-  const rocket5 = new PIXI.AnimatedSprite(raceLoopRocketTextures[4]);
+  const rocket5 = new Sprite(raceLoopRocketTextures[4]);
   rocket5.x = -80;
 
-  const rocket6 = new PIXI.AnimatedSprite(raceLoopRocketTextures[5]);
+  const rocket6 = new Sprite(raceLoopRocketTextures[5]);
   rocket6.x = 80;
 
-  const rocket7 = new PIXI.AnimatedSprite(raceLoopRocketTextures[6]);
+  const rocket7 = new Sprite(raceLoopRocketTextures[6]);
   rocket7.x = 220;
 
-  const rocket8 = new PIXI.AnimatedSprite(raceLoopRocketTextures[7]);
+  const rocket8 = new Sprite(raceLoopRocketTextures[7]);
   rocket8.x = 360;
 
-  const rocket9 = new PIXI.AnimatedSprite(raceLoopRocketTextures[8]);
+  const rocket9 = new Sprite(raceLoopRocketTextures[8]);
   rocket9.x = 500;
 
-  const rocket10 = new PIXI.AnimatedSprite(raceLoopRocketTextures[9]);
+  const rocket10 = new Sprite(raceLoopRocketTextures[9]);
   rocket10.x = -halfW + 1320;
 
-  raceLoopRocketsArr = new Array<{
-    player: PlayerRank;
-    rocket: PIXI.AnimatedSprite;
-    stopFrame: number;
-    oldDirection: "up" | "down";
-  }>();
+  raceLoopRocketsArr = new Array<RocketInfo>();
 
   playerRanks.forEach((element) => {
     if (element.className == "rocket-1") {
@@ -574,7 +540,9 @@ function createDeleteRaceLoopGroup(isDelete: boolean) {
         stopFrame: 0,
         oldDirection: "up",
       });
-    } else if (element.className == "rocket-2") {
+    }
+    
+    else if (element.className == "rocket-2") {
       raceLoopRocketsArr.push({
         player: element,
         rocket: rocket2,
@@ -645,13 +613,14 @@ function createDeleteRaceLoopGroup(isDelete: boolean) {
     element.rocket.anchor.set(0.5, 1);
     element.rocket.y = rocketY;
     element.rocket.loop = false;
+    element.rocket.visible = true;
     element.rocket.animationSpeed = gameAnimSpeed;
     element.rocket.play();
     element.rocket.onFrameChange = (cFrame: number) => {
       // console.log(num, element.player.className)
       if (isRocketRankChangeAllowed) {
         if (cFrame == element.stopFrame) {
-          element.rocket.stop();
+          //element.rocket.stop();
         }
       }
     };
@@ -685,6 +654,59 @@ function createDeleteRaceLoopGroup(isDelete: boolean) {
   };
 
   world.addChild(raceLoopContainer);
+
+  app.ticker.add((delta) => {
+    for (const element of raceLoopRocketsArr) {
+      element.rocket.updateMe(delta / 60);
+    }
+  });
+}
+
+export function updateGamePlayerRanks(ranks: Array<PlayerRank>) {
+  // console.log("game");
+  if (!isRocketRankChangeAllowed) {
+    print("Rank change not allowed");
+    return;
+  }
+
+  const frameInv = Math.floor(30 / ranks.length);
+
+  for (const element of raceLoopRocketsArr) {
+    const index = ranks.findIndex((item) => item.name == element.player.name);
+    if (index != -1) {
+      const oldPos = (ranks.length - element.player.rank) * frameInv;
+      const gotoFrame = (ranks.length - ranks[index].rank) * frameInv;
+      // console.log(element.player.name, gotoFrame, ranks[index].rank);
+      let textures = Array();
+
+      if (element.player.className == "rocket-1") {
+        textures = raceLoopRocketTextures[0].slice();
+      } else if (element.player.className == "rocket-2") {
+        textures = raceLoopRocketTextures[1].slice();
+      } else if (element.player.className == "rocket-3") {
+        textures = raceLoopRocketTextures[2].slice();
+      } else if (element.player.className == "rocket-4") {
+        textures = raceLoopRocketTextures[3].slice();
+      } else if (element.player.className == "rocket-5") {
+        textures = raceLoopRocketTextures[4].slice();
+      } else if (element.player.className == "rocket-6") {
+        textures = raceLoopRocketTextures[5].slice();
+      } else if (element.player.className == "rocket-7") {
+        textures = raceLoopRocketTextures[6].slice();
+      } else if (element.player.className == "rocket-8") {
+        textures = raceLoopRocketTextures[7].slice();
+      } else if (element.player.className == "rocket-9") {
+        textures = raceLoopRocketTextures[8].slice();
+      } else if (element.player.className == "rocket-10") {
+        textures = raceLoopRocketTextures[9].slice();
+      }
+      const newRank = Math.floor(Math.random() * 9 + 1);
+      //goToFrameAndPlay(element, element.player.rank, newRank, textures);
+      element.rocket.timeToReach = 1.5;
+      element.rocket.playTo(gotoFrame);
+      element.player.rank = newRank;
+    }
+  }
 }
 
 function createDeleteRocketFinishGroup(isDelete: boolean) {
@@ -818,4 +840,46 @@ function showBoundingBox(
 
 function print(msg: string) {
   if (isTestingMode) console.log(msg);
+}
+
+function goToFrameAndPlay(
+  info: RocketInfo,
+  oldRank: number,
+  newRank: number,
+  textures: any[]
+) {
+  const maxFrame = 29;
+  const maxRank = 10;
+
+  const isUp = newRank > oldRank;
+  const frameInv = Math.floor(30 / maxRank);
+  const oldPos = maxFrame * (oldRank / maxRank);
+  const newPos = maxFrame * (newRank / maxRank);
+
+  console.log({ oldRank, newRank, isUp, oldPos, newPos, isRev: info.isRev });
+
+  if (newRank > oldRank) {
+    //up
+    //rev must be false
+    if (info.isRev) {
+      info.isRev = false;
+      info.rocket.textures = textures;
+      info.rocket.gotoAndPlay(maxFrame - oldPos);
+    } else {
+      info.rocket.gotoAndPlay(oldPos);
+    }
+    info.stopFrame = newPos;
+  } else if (newRank < oldRank) {
+    //down
+    if (!info.isRev) {
+      //reverse it
+      info.isRev = true;
+      info.rocket.textures = textures.reverse();
+      info.rocket.gotoAndPlay(maxFrame - oldPos);
+    } else {
+      info.rocket.gotoAndPlay(oldPos);
+    }
+    //if rev
+    info.stopFrame = newPos;
+  }
 }
